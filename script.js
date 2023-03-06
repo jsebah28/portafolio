63f7efc2a3391a714d707162020bc7fc15098b6c
jsebah28 <106888673+jsebah28@users.noreply.github.com>
2023-03-06 09:34:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeContent() {
+    document.getElementById('proyect_description_1').innerHTML = 'Este proyecto es de total autoria de la empresa referenciada y mostrarlo aquí solo tiene intención informativa';
+}
+
+function changeContent1() {
+    document.getElementById('proyect_description_1').innerHTML = '';
+}
+
+function changeContent3() {
+    document.getElementById('proyect_description_2').innerHTML = '';
+}
+
+function changeContent2() {
+    document.getElementById('proyect_description_2').innerHTML = 'Este proyecto es de total autoria de la empresa referenciada y mostrarlo aquí solo tiene intención informativa';
+}
